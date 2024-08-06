bbcb7e2816a347e6ae62faa7361afee3cd4884fc
MisaZivanovic <eyes-of-wrath@hotmail.com>
2024-08-06 16:50:23 +0200

    Some changes

--- groqServer.py
+++ groqServer.py
@@ -2,7 +2,7 @@ from groq import Groq
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-
+api_key="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"
 
 
 app = Flask(__name__)
